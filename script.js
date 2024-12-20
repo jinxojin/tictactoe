15b868ccb059c67c731b2e25deb5d66e28f51f2d
jinxojin <kepsee@icloud.com>
2024-12-20 23:55:39 +0000

    Added functions for checking if a player has won horizontally or vertically

---
+++ script.js
@@ -0,0 +1,66 @@
+const tilesCollection = document.getElementsByClassName("tile");
+const tiles = [...tilesCollection];
+
+let isPlayerX = true;
+
+const checkHorizontal = () => {
+  const tileArr = [0, 3, 6];
+
+  for (let i = 0; i < 3; i++) {
+    const tempArr = [];
+    let tempLetter = 0;
+
+    for (let j = 0; j < 3; j++) {
+      tempArr.push(tiles[tileArr[i] + j].textContent);
+    }
+
+    if (
+      tempArr[0] === tempArr[1] &&
+      tempArr[0] === tempArr[2] &&
+      tempArr[0] !== ""
+    ) {
+      alert("horizontal");
+    }
+  }
+};
+
+const checkVertical = () => {
+  for (let i = 0; i < 3; i++) {
+    const tempArr = [];
+
+    for (let j = 0; j < 9; j += 3) {
+      tempArr.push(tiles[i + j].textContent);
+    }
+
+    console.log(tempArr);
+
+    if (
+      tempArr[0] !== "" &&
+      tempArr[0] === tempArr[1] &&
+      tempArr[0] === tempArr[2]
+    ) {
+      alert("vertical");
+      return true;
+    }
+  }
+  return false;
+};
+
+const draw = (event) => {
+  const el = event.target;
+  if (el.textContent.length === 0) {
+    if (isPlayerX) {
+      el.textContent = "x";
+      isPlayerX = !isPlayerX;
+    } else {
+      el.textContent = "o";
+      isPlayerX = !isPlayerX;
+    }
+    checkHorizontal();
+    checkVertical();
+  }
+};
+
+tiles.forEach((el, index) => {
+  el.addEventListener("click", draw);
+});
